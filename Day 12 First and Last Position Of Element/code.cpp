0e62c2d5cd16712c5c541a7137499b50f7876599
divyeagarwal <divyeagarwal436@gmail.com>
2026-01-12 23:52:31 +0530

    Implement binary search for first and last positions of target in sorted array; update output image

--- Day 12 First and Last Position Of Element/code.cpp
+++ Day 12 First and Last Position Of Element/code.cpp
@@ -1,17 +1,50 @@
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& nums, int target) {
-        int n=nums.size();
-        vector<int> arr;
-        int first=-1,last=-1;
-        for(int i=0;i<n;i++){
-            if(nums[i]==target){
-                if(first==-1) first=i;
-                last=i;
+    int first(vector<int> &arr,int n,int target){
+        int first=-1;
+        int low=0,high=n-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(arr[mid]==target){
+                first=mid;
+                high=mid-1;
+            }else if(arr[mid]<target){
+                low=mid+1;
+            }else{
+                high=mid-1;
             }
         }
-        arr.push_back(first);
-        arr.push_back(last);
-        return arr;
+        return first;
+    }
+    int last(vector<int> &arr,int n,int target){
+        int last=-1;
+        int low=0,high=n-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(arr[mid]==target){
+                last=mid;
+                low=mid+1;
+            }else if(arr[mid]<target){
+                low=mid+1;
+            }else{
+                high=mid-1;
+            }
+        }
+        return last;
+    }
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int n=nums.size();
+        vector<int> ans;
+        int first1=first(nums,n,target);
+        if(first1==-1){
+            ans.push_back(-1);
+            ans.push_back(-1);
+        }else {
+            ans.push_back(first1);
+            int last1=last(nums,n,target);
+            ans.push_back(last1);
+        }
+        return ans;
+        
     }
 };
