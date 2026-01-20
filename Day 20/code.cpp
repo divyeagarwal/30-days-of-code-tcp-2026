725c8219d6a9bd3ca92da52e8e350554810177b1
divyeagarwal <divyeagarwal436@gmail.com>
2026-01-20 23:40:29 +0530

    Day 20

---
+++ Day 20/code.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if(head==NULL || head->next==NULL) return head;
+        ListNode* odd=head;
+        ListNode* even=head->next;
+        ListNode* evenhead=head->next;
+        while(even && even->next){
+            odd->next=odd->next->next;
+            even->next=even->next->next;
+            odd=odd->next;
+            even=even->next;
+        }
+        odd->next=evenhead;
+        return head;
+    }
+};
