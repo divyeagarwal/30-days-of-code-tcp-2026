8d5cadcb163f40374765f144da366b70d905eaff
divyeagarwal <divyeagarwal436@gmail.com>
2026-01-30 22:38:42 +0530

    Day 30

---
+++ Day 30/code.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    vector<int> intersection(vector<int>& a, vector<int>& b) {
+        int n = a.size(), m = b.size();
+        int i = 0, j = 0;
+        vector<int> res;
+        while (i < n && j < m) {
+            if (i > 0 && a[i] == a[i - 1]) {
+                i++;
+                continue;
+            }
+            if (a[i] < b[j]) {
+                i++;
+            } else if (a[i] > b[j]) {
+                j++;
+            } else {
+                res.push_back(a[i]);
+                i++;
+                j++;
+            }
+        }
+        return res;
+    }
+};
